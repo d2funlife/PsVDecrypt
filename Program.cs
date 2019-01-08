b169b309f16a1a1f479de1d71b5c0f56a50650f4
Danil Pavlov <d2funlife@gmail.com>
2019-01-08 17:28:25 +0300

    Fix decoding
    fix dest folder name as course name;
    fix path length error for some courses;

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Data.SQLite;
-using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace PsVDecrypt
 {
@@ -43,12 +43,7 @@ namespace PsVDecrypt
             Console.WriteLine("Output Directory: " + OutputDir);
 
             var subdirs = Directory.GetDirectories(coursesdir);
-            Console.WriteLine("\nFound " + subdirs.Length + " course(s):");
-
-            foreach (var subdir in subdirs)
-            {
-                Console.WriteLine(" > " + Path.GetFileName(subdir));
-            }
+            Console.WriteLine("\nFound " + subdirs.Length + " course(s)");
 
             if (!Directory.Exists(OutputDir))
             {
@@ -65,26 +60,13 @@ namespace PsVDecrypt
             }
 
             Console.WriteLine(" > All done.\n");
+            Console.ReadLine();
         }
 
         private static void DecryptCourse(string courseSrcDir)
         {
             var courseName = Path.GetFileName(courseSrcDir);
-            var courseDstDir = Path.Combine(OutputDir, courseName);
             var hasTranscript = false;
-            Console.WriteLine("Processing course " + courseName + " ..");
-
-            // Reset Directory
-            if (Directory.Exists(courseDstDir))
-            {
-                Util.DeleteDirectory(courseDstDir);
-            }
-            Util.CreateDirectory(courseDstDir);
-
-
-            // Copy Image
-            File.Copy(Path.Combine(courseSrcDir, "image.jpg"), Path.Combine(courseDstDir, "image.jpg"));
-            Console.WriteLine(" > Done copying course image.");
 
             // Read Course Info
             var command =
@@ -101,115 +83,146 @@ namespace PsVDecrypt
 
             hasTranscript = (long) dataTable.Rows[0]["HasTranscript"] == 1;
 
-            // Save Course Info to JSON
-            File.WriteAllText(Path.Combine(courseDstDir, "course-info.json"),
-                JsonConvert.SerializeObject(dataTable, Formatting.Indented));
-            Console.WriteLine(" > Done saving course info.");
+            var invalidFileChars = Path.GetInvalidFileNameChars();
+            var realCourseName = String.Concat(((String)dataTable.Rows[0]["Title"]).Where(x=> !invalidFileChars.Contains(x)));
+            var courseDstDir = Path.Combine(OutputDir, realCourseName);
+            Console.WriteLine("Processing course " + realCourseName + " ..");
 
-            // Read Module Info
-            command = new SQLiteCommand("select * from Module where CourseName=@CourseName", _dbConn)
+            // Reset Directory
+            if (Directory.Exists(courseDstDir))
             {
-                CommandType = CommandType.Text
-            };
-            command.Parameters.Add(new SQLiteParameter("@CourseName", courseName));
-            reader = command.ExecuteReader();
-            dataTable = new DataTable();
-            dataTable.Load(reader);
-            Console.WriteLine(" > Found " + dataTable.Rows.Count + " module(s).");
-            var dataTableAsList =
-                JsonConvert.DeserializeObject<List<object>>(JsonConvert.SerializeObject(dataTable));
+                return;
+            }
+            Util.CreateDirectory(courseDstDir);
 
-            // Process Each Module
-            for (var i = 0; i < dataTable.Rows.Count; i++)
+            try
             {
-                var moduleItem = dataTable.Rows[i];
-                Console.WriteLine("   > Processing module: " + moduleItem["Title"]);
+                // Copy Image
+                File.Copy(Path.Combine(courseSrcDir, "image.jpg"), Path.Combine(courseDstDir, "image.jpg"));
+                Console.WriteLine(" > Done copying course image.");
 
-                // Get Module Dir
-                var moduleHash = Util.GetModuleHash(moduleItem["Name"] as string,
-                    moduleItem["AuthorHandle"] as string);
-                var moduleSrcDir = Path.Combine(courseSrcDir, moduleHash);
-                var moduleDstDir = Path.Combine(courseDstDir,
-                    (moduleItem["ModuleIndex"].ToString()).PadLeft(2, '0') + "." +
-                    Util.TitleToFileName(moduleItem["Title"] as string));
-                if (!Directory.Exists(moduleDstDir))
-                    Util.CreateDirectory(moduleDstDir);
+                // Save Course Info to JSON
+                File.WriteAllText(Path.Combine(courseDstDir, "course-info.json"),
+                    JsonConvert.SerializeObject(dataTable, Formatting.Indented));
+                Console.WriteLine(" > Done saving course info.");
 
-                // Save Module Info to JSON
-                File.WriteAllText(Path.Combine(moduleDstDir, "module-info.json"),
-                    JsonConvert.SerializeObject(dataTableAsList[i], Formatting.Indented));
-                Console.WriteLine("     > Done saving module info.");
-
-                // Read Clip Info
-                var clipsCommand =
-                    new SQLiteCommand("select * from Clip where ModuleId=@ModuleId", _dbConn)
-                    {
-                        CommandType = CommandType.Text
-                    };
-                clipsCommand.Parameters.Add(new SQLiteParameter("@ModuleId", moduleItem["Id"]));
-                var clipsReader = clipsCommand.ExecuteReader();
-                var clipsDataTable = new DataTable();
-                clipsDataTable.Load(clipsReader);
-
-                // Save Clips Info to JSON
-                File.WriteAllText(Path.Combine(moduleDstDir, "clips-info.json"),
-                    JsonConvert.SerializeObject(clipsDataTable, Formatting.Indented));
-                Console.WriteLine("     > Done saving clips info.");
-
-                // Process Each Clip
-                for (var j = 0; j < clipsDataTable.Rows.Count; j++)
+                // Read Module Info
+                command = new SQLiteCommand("select * from Module where CourseName=@CourseName", _dbConn)
                 {
-                    var clipItem = clipsDataTable.Rows[j];
-                    Console.WriteLine("     > Processing clip: " + clipItem["Title"]);
-                    var clipSrc = Path.Combine(moduleSrcDir, (string) clipItem["Name"]) + ".psv";
-                    var clipDst = Path.Combine(moduleDstDir,
-                                      (clipItem["ClipIndex"].ToString()).PadLeft(2, '0') + "." +
-                                      Util.TitleToFileName((string) clipItem["Title"])) + ".mp4";
+                    CommandType = CommandType.Text
+                };
+                command.Parameters.Add(new SQLiteParameter("@CourseName", courseName));
+                reader = command.ExecuteReader();
+                dataTable = new DataTable();
+                dataTable.Load(reader);
+                Console.WriteLine(" > Found " + dataTable.Rows.Count + " module(s).");
+                var dataTableAsList =
+                    JsonConvert.DeserializeObject<List<object>>(JsonConvert.SerializeObject(dataTable));
 
-                    // Decrypt Clip
-                    Util.DecryptFile(clipSrc, clipDst);
-                    Console.WriteLine("       > Done decrypting clip.");
+                // Process Each Module
+                for (var i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    var moduleItem = dataTable.Rows[i];
+                    Console.WriteLine("   > Processing module: " + moduleItem["Title"]);
 
-                    // Save Transcript
-                    if (!hasTranscript) continue;
-                    var transcriptsCommand =
-                        new SQLiteCommand("select * from ClipTranscript where ClipId=@ClipId", _dbConn)
+                    // Get Module Dir
+                    var moduleHash = Util.GetModuleHash(moduleItem["Name"] as string,
+                        moduleItem["AuthorHandle"] as string);
+                    var moduleSrcDir = Path.Combine(courseSrcDir, moduleHash);
+                    var moduleDstDir = Path.Combine(courseDstDir,
+                        (moduleItem["ModuleIndex"].ToString()).PadLeft(2, '0') + "." +
+                        Util.TitleToFileName(moduleItem["Title"] as string));
+                    if (!Directory.Exists(moduleDstDir))
+                        Util.CreateDirectory(moduleDstDir);
+
+                    // Save Module Info to JSON
+                    File.WriteAllText(Path.Combine(moduleDstDir, "module-info.json"),
+                        JsonConvert.SerializeObject(dataTableAsList[i], Formatting.Indented));
+                    Console.WriteLine("     > Done saving module info.");
+
+                    // Read Clip Info
+                    var clipsCommand =
+                        new SQLiteCommand("select * from Clip where ModuleId=@ModuleId", _dbConn)
                         {
                             CommandType = CommandType.Text
                         };
-                    transcriptsCommand.Parameters.Add(new SQLiteParameter("@ClipId", clipItem["Id"]));
-                    var transcriptsReader = transcriptsCommand.ExecuteReader();
-                    var transcriptsDataTable = new DataTable();
-                    transcriptsDataTable.Load(transcriptsReader);
-                    if (transcriptsDataTable.Rows.Count == 0) continue;
+                    clipsCommand.Parameters.Add(new SQLiteParameter("@ModuleId", moduleItem["Id"]));
+                    var clipsReader = clipsCommand.ExecuteReader();
+                    var clipsDataTable = new DataTable();
+                    clipsDataTable.Load(clipsReader);
 
-                    // Generate Srt File
-                    var sb = new StringBuilder();
-                    var sequenceI = 0;
-                    foreach (DataRow transcriptItem in transcriptsDataTable.Rows)
+                    // Save Clips Info to JSON
+                    File.WriteAllText(Path.Combine(moduleDstDir, "clips-info.json"),
+                        JsonConvert.SerializeObject(clipsDataTable, Formatting.Indented));
+                    Console.WriteLine("     > Done saving clips info.");
+
+                    // Process Each Clip
+                    for (var j = 0; j < clipsDataTable.Rows.Count; j++)
                     {
-                        sequenceI++;
-                        sb.Append(sequenceI + "\n");
+                        var clipItem = clipsDataTable.Rows[j];
+                        Console.WriteLine("     > Processing clip: " + clipItem["Title"]);
+                        var clipSrc = Path.Combine(moduleSrcDir, (string)clipItem["Name"]) + ".psv";
+                        
+                        var videoName = Path.Combine(moduleDstDir,
+                                          (clipItem["ClipIndex"].ToString()).PadLeft(2, '0') + "." +
+                                          Util.TitleToFileName((string)clipItem["Title"]));
 
-                        var startMs = (long) transcriptItem["StartTime"];
-                        var endMs = (long) transcriptItem["EndTime"];
-                        var startTime = TimeSpan.FromMilliseconds(startMs);
-                        var endTime = TimeSpan.FromMilliseconds(endMs);
-                        sb.Append(startTime.ToString(@"hh\:mm\:ss") + "," + (startMs % 1000));
-                        sb.Append(" --> ");
-                        sb.Append(endTime.ToString(@"hh\:mm\:ss") + "," + (endMs % 1000));
-                        sb.Append("\n");
+                        if(videoName.Length + 4 > 255)
+                        {
+                            videoName = videoName.Substring(0, 251);
+                        }
 
-                        sb.Append(string.Join("\n",
-                            ((string) transcriptItem["Text"]).Replace("\r", "").Split('\n')
-                            .Select(text => "- " + text)));
-                        sb.Append("\n\n");
+                        var clipDst = videoName + ".mp4";
+
+                        // Decrypt Clip
+                        Util.DecryptFile(clipSrc, clipDst);
+                        Console.WriteLine("       > Done decrypting clip.");
+
+                        // Save Transcript
+                        if (!hasTranscript) continue;
+                        var transcriptsCommand =
+                            new SQLiteCommand("select * from ClipTranscript where ClipId=@ClipId", _dbConn)
+                            {
+                                CommandType = CommandType.Text
+                            };
+                        transcriptsCommand.Parameters.Add(new SQLiteParameter("@ClipId", clipItem["Id"]));
+                        var transcriptsReader = transcriptsCommand.ExecuteReader();
+                        var transcriptsDataTable = new DataTable();
+                        transcriptsDataTable.Load(transcriptsReader);
+                        if (transcriptsDataTable.Rows.Count == 0) continue;
+
+                        // Generate Srt File
+                        var sb = new StringBuilder();
+                        var sequenceI = 0;
+                        foreach (DataRow transcriptItem in transcriptsDataTable.Rows)
+                        {
+                            sequenceI++;
+                            sb.Append(sequenceI + "\n");
+
+                            var startMs = (long)transcriptItem["StartTime"];
+                            var endMs = (long)transcriptItem["EndTime"];
+                            var startTime = TimeSpan.FromMilliseconds(startMs);
+                            var endTime = TimeSpan.FromMilliseconds(endMs);
+                            sb.Append(startTime.ToString(@"hh\:mm\:ss") + "," + (startMs % 1000));
+                            sb.Append(" --> ");
+                            sb.Append(endTime.ToString(@"hh\:mm\:ss") + "," + (endMs % 1000));
+                            sb.Append("\n");
+
+                            sb.Append(string.Join("\n",
+                                ((string)transcriptItem["Text"]).Replace("\r", "").Split('\n')
+                                .Select(text => "- " + text)));
+                            sb.Append("\n\n");
+                        }
+
+
+                        File.WriteAllText(clipDst + ".srt", sb.ToString());
+                        Console.WriteLine("       > Done saving subtitles.");
                     }
-
-
-                    File.WriteAllText(clipDst + ".srt", sb.ToString());
-                    Console.WriteLine("       > Done saving subtitles.");
                 }
+            }
+            catch (Exception exception)
+            {
+                Util.DeleteDirectory(courseDstDir);
             }
         }
     }
